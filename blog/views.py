34d7b948cd735f27085ee2981132a75e1d6609d9
nkyomuhendo <nkyomuhendo@gmail.com>
2023-10-30 13:37:54 +0300

    Added about page, made the blog a default homepage of the project

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('<h1>Hello Blogger, Welcome! </h1>')
+    return HttpResponse('<h1>Blog Home!</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>Blog About!</h1>')
