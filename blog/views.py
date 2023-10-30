a1a3c113a6506f88184ee2cf374eca9a892a71d1
nkyomuhendo <nkyomuhendo@gmail.com>
2023-10-30 14:56:57 +0300

    Added capability to pass variables to our templates via context dictionaries and implemented if conditionals for selective display and for loops

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,29 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'NicholasCK',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 29, 2023'
+    },
+    {
+        'author':'Johnie Cage',
+        'title':'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 30, 2023'
+    }
+]
+
+
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title': 'About'})
