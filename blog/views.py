967d8b062b493edfd0a3e1a834bbfe1066403010
nkyomuhendo <nkyomuhendo@gmail.com>
2023-10-31 13:44:06 +0300

    Added date formatting to home page posts, added our Post custom model to admin app and page for management

--- blog/views.py
+++ blog/views.py
@@ -2,22 +2,6 @@ from django.shortcuts import render
 from .models import Post
 
 
-posts = [
-    {
-        'author': 'NicholasCK',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 29, 2023'
-    },
-    {
-        'author':'Johnie Cage',
-        'title':'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 30, 2023'
-    }
-]
-
-
 def home(request):
     context = {
         'posts': Post.objects.all()
