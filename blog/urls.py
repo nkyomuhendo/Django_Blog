e988337e2ce62819b6de6775d511d8c35c670163
nkyomuhendo <nkyomuhendo@gmail.com>
2023-11-06 23:52:13 +0300

    Added PostCreateView which inherits from the CreateView, then overrode it's method form_valid() to cater for validation using the logged in user, created it's required template post_form.html which expects a form as context but can otherwise inherit from say the register template except for afew naming changes. The post model was also edited to include a get_absolute_url method so as to return an absolute path to that particular post ---using the django urls module of reverse vs redirect since it gives a full url to that route / path and let's the view handle the redirection.

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import (PostListView,
+                    PostDetailView,
+                    PostCreateView
+                    )
 from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
