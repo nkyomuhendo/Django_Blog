c26d095db8d2a90dfcd75c9f6512aff02e61b496
nkyomuhendo <nkyomuhendo@gmail.com>
2023-11-06 21:09:35 +0300

    Added another django CBV, DetailView, this time using all recommended generics from template to context names hence reducing the code base or burden to execute the functionality. We also created a url path that expects an integer and specified pk for the post id and finally we created links to the post detail by editing the home.html template to include dynamic links.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PostListView
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -16,6 +16,11 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app> / <model> _ <viewtype>.html i.e in our case log/post_list.html
     context_object_name = 'posts'
     ordering = '-date_posted'
+
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
