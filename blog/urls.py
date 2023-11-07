e6d86b19df4e7da49b4be3b410e01641eabce5c9
nkyomuhendo <nkyomuhendo@gmail.com>
2023-11-07 11:03:05 +0300

    Added a PostUpdateView that inherits from django CBV UpdateView, added extar controls to restrict updating of posts to only the author of the post using a django mixin UserPassesTestMixin and overode it's  method test_func to test the mentioned condition, it's url was also created matching post detail with update keyword, it uses same form as createView so no template was created

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 from .views import (PostListView,
                     PostDetailView,
-                    PostCreateView
+                    PostCreateView,
+                    PostUpdateView
                     )
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
-    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
 ]
